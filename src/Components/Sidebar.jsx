6bbbe6fa8a2a9bdcffdd4c8a361579f8257db03c
David Sousa <david01sousa@Hotmail.com>
2022-09-28 12:30:17 -0300

    finalizando sidebar e começando post

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
   return(
@@ -10,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/57757828?v=4" />
+
         <strong>David Sousa</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine  size={20}/>
           Editar seu perfil
         </a>
       </footer>    
