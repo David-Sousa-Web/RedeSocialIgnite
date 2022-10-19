3cc8cd446b63dd8c3daebcf91c9471855f8eaf11
David Sousa <david01sousa@Hotmail.com>
2022-10-19 19:25:38 -0300

    terminando post

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,20 @@ const posts = [
     ],
     publishedAt: new Date('2022-05-03 20:00:00')
   },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa!!', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portal', },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
 ]
 
 
@@ -33,8 +47,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
