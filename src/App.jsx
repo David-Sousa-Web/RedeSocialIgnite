a3bd53ab14709ed5d1960702fd7c461b0882eda2
David Sousa <david01sousa@Hotmail.com>
2022-10-19 17:38:24 -0300

    add: array de objetos

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ import './Global.css'
 import styles from './App.module.css'
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa!!', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portal', },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+]
+
+
 export function App() {
   return (
     <div>
