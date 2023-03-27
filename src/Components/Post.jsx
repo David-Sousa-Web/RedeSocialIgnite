e8c45fb259681472e2398871c04e0df6a27c335c
David Sousa <david01sousa@Hotmail.com>
2023-03-27 17:34:05 -0300

    att post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -4,8 +4,15 @@ import styles from './Post.module.css'
 
 import { format, formatDistanceToNow, roundToNearestMinutes } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
+
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -14,6 +21,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function HandleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1])
+  }
 
   return(
     <article className={styles.post}>
@@ -41,7 +54,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={HandleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
@@ -54,9 +67,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
